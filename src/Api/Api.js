929e5bf12e316241120b33d05558723feccfca20
osamahalashrf <osamahalashrf@gmail.com>
2025-08-02 12:33:32 +0300

    update

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,5 +1,5 @@
 //Auth
-export const baseURL = `http://127.0.0.1:8000/api`;
+export const baseURL = `https://backendfore-commerce-production.up.railway.app/api`;
 
 export const REGISTER = "register";
 
